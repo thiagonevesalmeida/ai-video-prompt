d94e3f094cb47d06a5adb15902bba826cb46c98a
Thiago Neves Almeida <thiagonevesdealmeida@gmail.com>
2023-09-15 11:52:49 +0100

    Add request routes

--- upload-ai-server/src/server.ts
+++ upload-ai-server/src/server.ts
@@ -1,14 +1,12 @@
 import { fastify } from 'fastify';
+import { getAllPromptsRoute } from './routes/get-all-prompts';
 
-const app = fastify();
+const app = fastify()
+
+app.register(getAllPromptsRoute)
 
 app.listen({
 	port: 3333,
 }).then(() => {
-	console.log("Running HTTP server at 'http://localhost:3333'");
-})
-
-//GET route
-app.get('/', () => {
-	return "Hello World";
+	console.log("Running HTTP server at 'http://localhost:3333'")
 })
