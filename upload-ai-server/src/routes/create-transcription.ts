fd99045a10e1a538a45a80609f0b4e23ea547ad3
Thiago Neves Almeida <thiagonevesdealmeida@gmail.com>
2023-09-16 18:57:29 +0100

    add video transcription setups

--- upload-ai-server/src/routes/create-transcription.ts
+++ upload-ai-server/src/routes/create-transcription.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 import { z } from "zod";
+import { createReadStream } from "node:fs";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
 	app.post('/videos/:videoId/transcription', async (req, res) => {
+		// validate request data
 		const paramsSchema = z.object({
 			videoId: z.string().uuid(),
 		})
@@ -15,9 +17,20 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
 		const { videoId } = paramsSchema.parse(req.params) //receber parâmetro e validar se está seguindo a estrutura indicada por z.object
 		const { prompt } = bodySchema.parse(req.body) //receber Request Body e validar se está seguindo a estrutura indicada por z.object
 
+		// video transcription
+		const video = await prisma.video.findUniqueOrThrow({
+			where: {
+				id: videoId,
+			}
+		})
+		const videoPath = video.path
+
+		const audioReadStream = createReadStream(videoPath)
+
 		return {
 			videoId,
 			prompt,
+			videoPath
 		}
 	})
 }
