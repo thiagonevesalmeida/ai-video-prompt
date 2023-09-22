3628f66d3ecf5aefc97c45516c5b7d0c40874f10
Thiago Neves Almeida <thiagonevesdealmeida@gmail.com>
2023-09-22 17:34:58 +0100

    Update generate-ai-complete route props

--- upload-ai-server/src/routes/generate-ai-completion.ts
+++ upload-ai-server/src/routes/generate-ai-completion.ts
@@ -9,11 +9,11 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
 		// validate request data
 		const bodySchema = z.object({
 			videoId: z.string().uuid(),
-			template: z.string(),
+			prompt: z.string(),
 			temperature: z.number().min(0).max(1).default(0.5),
 		})
 
-		const { videoId, template ,temperature } = bodySchema.parse(request.body)
+		const { videoId, prompt ,temperature } = bodySchema.parse(request.body)
 
 		//AI completion
 		//procurar e retornar video, caso não retornar erro
@@ -28,7 +28,7 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
 			return reply.status(400).send({ error: "Video transcription was not generated yet."})
 		}
 		
-		const promptMessage = template.replace('{transcription}', video.transcription) //substituir parte da template message ('{transcription}') enviada no "Request Body" pelo video transcription gerado pela AI
+		const promptMessage = prompt.replace('{transcription}', video.transcription) //substituir parte da template message ('{transcription}') enviada no "Request Body" pelo video transcription gerado pela AI
 
 		// Chamada para OpenAI
 		const response = await openai.chat.completions.create({
